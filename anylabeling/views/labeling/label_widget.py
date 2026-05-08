64e1292bab2e6fb3485e9701047942bace6ef485
xusuyong <2209245477@qq.com>
2026-05-08 16:06:16 +0800

    懒加载UltralyticsDialog，提升启动速度

--- anylabeling/views/labeling/label_widget.py
+++ anylabeling/views/labeling/label_widget.py
@@ -36,7 +36,6 @@ from PyQt6.QtWidgets import (
 
 from anylabeling.services.auto_labeling.types import AutoLabelingMode
 from anylabeling.services.auto_labeling import _THUMBNAIL_RENDER_MODELS
-from anylabeling.views.training import UltralyticsDialog
 
 from ...app_info import (
     __appname__,
@@ -3402,6 +3401,8 @@ class LabelingWidget(LabelDialog):
 
     # Trainer
     def start_training(self, mode):
+        from anylabeling.views.training import UltralyticsDialog
+
         if mode != "ultralytics":
             return
 
